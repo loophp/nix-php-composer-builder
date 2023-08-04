21addbcb57afe5a5491ec7c2f86b77a939f60b4a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-08-04 17:41:45 +0200

    use an overlay

--- flake.nix
+++ flake.nix
@@ -1,28 +1,34 @@
 {
   description = "PHP Composer builder";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs = { };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake =
         {
-          api.buildComposerProject = pkgs: php:
+          overlays.default = final: prev:
             let
-              composerHooks = pkgs.callPackages ./nix/hooks/default.nix { inherit php; };
+              buildComposerProject = php:
+                let
+                  composerHooks = prev.callPackages ./nix/hooks/default.nix { inherit php; };
 
-              composer-local-repo-plugin = pkgs.callPackage ./nix/composer-local-repo-plugin.nix {
-                composer = php.packages.composer;
-              };
+                  composer-local-repo-plugin = prev.callPackage ./nix/composer-local-repo-plugin.nix {
+                    composer = php.packages.composer;
+                  };
 
-              mkComposerRepository = pkgs.callPackage ./nix/build-composer-repository.nix {
-                inherit php composer-local-repo-plugin composerHooks;
-              };
+                  mkComposerRepository = prev.callPackage ./nix/build-composer-repository.nix {
+                    inherit php composer-local-repo-plugin composerHooks;
+                  };
+                in
+                prev.callPackage ./nix/build-composer-project.nix {
+                  inherit php composer-local-repo-plugin mkComposerRepository composerHooks;
+                };
             in
-            pkgs.callPackage ./nix/build-composer-project.nix {
-              inherit php composer-local-repo-plugin mkComposerRepository composerHooks;
+            {
+              api = {
+                inherit buildComposerProject;
+              };
             };
         };
     };
