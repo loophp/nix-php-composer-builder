17d97d57b6d4df76c6963cef466c6c9554e3f00b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-08-07 15:06:40 +0200

    chore: update default template
    Add a welcome text

--- flake.nix
+++ flake.nix
@@ -10,6 +10,37 @@
           basic = {
             path = ./templates/basic;
             description = "A basic container for getting started with PHP development";
+            welcomeText = ''
+              # Simple PHP/Composer Template
+
+              ## Intended usage
+
+              This template is designed to provide a basic flake template for
+              PHP development.
+
+              Features of this template include:
+
+              - A default `PHP development shell` with PHP and Composer.
+                Use it with `nix develop .`
+              - The `composer` flake application
+                Use it with `nix run .#composer -- --version`
+              - The `satis` package provided as example on how to bundle a PHP
+                application
+              - The `satis` flake application provided as example
+                Use it with `nix run .#satis -- --version`
+
+              For each features, Nix will detect the required extensions for PHP
+              by reading the `composer.json` file. As long as your
+              `composer.json` file list the required extensions in the
+              `require` or `require-dev` sections, no any other configuration is
+              needed on your part.
+
+              If you need to modify the PHP configuration, create a file
+              `.user.ini` in the project with your custom PHP configuration
+              directives.
+
+              Happy coding !
+            '';
           };
         };
 
