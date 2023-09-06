600bc67c1adce95675d6c3487ba61ae5fab4dc25
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-09-06 11:56:04 +0200

    template: fix database handling

--- templates/basic/flake.nix
+++ templates/basic/flake.nix
@@ -41,13 +41,19 @@
               ];
 
               text = ''
-                MKTEMP=$(mktemp -u)
-                TMPDIR=$(dirname "$MKTEMP")
-                APP_CACHE_DIR=$TMPDIR/cache
+                APP_CACHE_DIR=$(mktemp -u)/cache
                 APP_LOG_DIR=$APP_CACHE_DIR/log
-                export TMPDIR
+                DATABASE_URL=sqlite:///$APP_CACHE_DIR/database.sqlite
+
                 export APP_CACHE_DIR
                 export APP_LOG_DIR
+                export DATABASE_URL
+
+                mkdir -p "$APP_CACHE_DIR"
+                mkdir -p "$APP_LOG_DIR"
+
+                cp -f ${self'.packages.symfony-demo}/share/php/symfony-demo/data/database.sqlite "$APP_CACHE_DIR"/database.sqlite
+                chmod +w "$APP_CACHE_DIR"/database.sqlite
 
                 ${lib.getExe pkgs.symfony-cli} serve --document-root ${self'.packages.symfony-demo}/share/php/symfony-demo/public --allow-http
               '';
